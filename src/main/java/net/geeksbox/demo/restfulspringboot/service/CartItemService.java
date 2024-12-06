3503fa234eb91a4c3d1a0df0ec5abde5ecbaf8d6
nguyenanhhh <honganhnguyen08102003@gmail.com>
2024-12-06 20:53:30 +0700

    delete

--- src/main/java/net/geeksbox/demo/restfulspringboot/service/CartItemService.java
+++
@@ -1,8 +0,0 @@
-package net.geeksbox.demo.restfulspringboot.service;
-
-import net.geeksbox.demo.restfulspringboot.model.CartItem;
-
-public interface CartItemService {
-	public CartItem addProduct(CartItem product);
-	
-}
--- src/main/java/net/geeksbox/demo/restfulspringboot/service/OrderItemEnityService.java
+++
@@ -1,9 +0,0 @@
-package net.geeksbox.demo.restfulspringboot.service;
-
-import net.geeksbox.demo.restfulspringboot.model.CartItem;
-import net.geeksbox.demo.restfulspringboot.model.OrderEnity;
-import net.geeksbox.demo.restfulspringboot.model.OrderItemEnity;
-
-public interface OrderItemEnityService {
-	public OrderItemEnity addorder(OrderItemEnity orderEnity);
-}
